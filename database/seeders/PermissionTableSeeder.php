890f1f6b49e6211c04eb02fb21dc60d631f69618
Aries Diolazo <j.ariesdiolazo.com>
2022-05-18 02:50:30 +0800

    roles

---
+++ database/seeders/PermissionTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+            'role-list',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'role-access',
+            'product-list',
+            'product-create',
+            'product-edit',
+            'product-delete',
+            'product-access',
+            'permission-create',
+            'permission-edit',
+            'permission-list',
+            'permission-delete',
+            'permission-access'
+
+         ];
+      
+         foreach ($permissions as $permission) {
+              Permission::create(['name' => $permission]);
+         }
+    }
+}
